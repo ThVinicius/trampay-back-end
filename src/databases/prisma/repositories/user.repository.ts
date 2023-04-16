376951674aaf331f1f85dec4b472d913d13927cb
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-04-16 17:37:48 -0300

    refactor: file name

--- src/databases/prisma/repositories/user.repository.ts
+++ src/databases/prisma/repositories/user.repository.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
 import { UserRepositoryInterface } from 'src/user/user.repository.interface';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class UserRepository implements UserRepositoryInterface {
   constructor(private prisma: PrismaService) {}
 
-  public async create(data: Omit<User, 'id' | 'createdAt'>): Promise<User> {
+  public async create(data: UserEntity): Promise<User> {
     return await this.prisma.user.create({ data });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.prisma.user.findUnique({ where: { email } });
   }
 }
